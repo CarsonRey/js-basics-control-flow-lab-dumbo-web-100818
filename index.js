4a9a9b578a7340fbeea97eb6856b11233e1ca7a7
CarsonRey <c.rey494@gmail.com>
2018-11-18 11:12:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  }
+
+  else if (1999 < someValue && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city) {
+city == 'NYC' ? (response = 'Ok, sounds good.') : (response = 'No go.');
+
+return response;
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default: return 'Bye.'
+
+  }
+
+}
